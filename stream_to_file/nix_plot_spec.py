6ac88a7e0eca1a3172d671136eab08caa090db9c
weygoldt <88969563+weygoldt@users.noreply.github.com>
2023-04-16 23:43:25 +0200

    some refactoring

--- stream_to_file/nix_plot_spec.py
+++ stream_to_file/nix_plot_spec.py
@@ -2,14 +2,16 @@ import pathlib
 
 import matplotlib.pyplot as plt
 import nixio as nio
+from IPython import embed
 
 
 def main():
-    path = pathlib.Path("dataset.nix")
+    path = pathlib.Path("../../chirpdetector-cnn/real_data/dataset.nix")
     with nio.File.open(str(path), nio.FileMode.ReadOnly) as f:
-        spec = f.blocks[0].data_arrays["spec"][:]
-        time = f.blocks[0].data_arrays["time"][:]
-        freq = f.blocks[0].data_arrays["freq"][:]
+        # embed()g
+        spec = f.blocks[0].data_arrays["spec"][:, :20000]
+        time = f.blocks[0].data_arrays["spec_time"][:20000]
+        freq = f.blocks[0].data_arrays["spec_freq"][:]
 
         print(spec.shape)
         print(time.shape)
