29d7a9bb7af37c02ce7ecc76378dd1a1b013b4d6
weygoldt <88969563+weygoldt@users.noreply.github.com>
2023-05-03 16:21:10 +0200

    added gpu support

--- spectrogram_inversion/invert_spectrogram.py
+++ spectrogram_inversion/invert_spectrogram.py
@@ -1,6 +1,5 @@
 import pathlib
 
-import librosa
 import matplotlib.pyplot as plt
 import numpy as np
 import requests
@@ -15,8 +14,10 @@ from thunderfish.fakefish import chirps, wavefish_eods
 # chps1, amp1 = chirps(eodf=700.0, samplerate=44100.0, duration=1.0, chirp_freq=0,
 #            chirp_size=100.0, chirp_width=0.01, chirp_kurtosis=1.0, chirp_contrast=0.05)
 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
 chps2, amp2 = chirps(
-    eodf=500.0,
+    eodf=0,
     samplerate=44100.0,
     duration=1.0,
     chirp_freq=5.0,
@@ -26,9 +27,13 @@ chps2, amp2 = chirps(
     chirp_contrast=0.05,
 )
 
+bl1 = np.linspace(500, 600, 44100)
+chps2 = bl1 + chps2
+freqs1 = np.linspace(550, 500, 44100)
+
 eod1 = wavefish_eods(
     fish="Alepto",
-    frequency=700,
+    frequency=freqs1,
     samplerate=44100.0,
     duration=1.0,
     phase0=0.0,
@@ -61,36 +66,60 @@ time = np.arange(0, 1, 1 / 44100)
 # eod = eod[(time > 68) & (time < 70)]
 
 # compute the spectrogram with pytorch
+nfft = 4096
+# hop_length = int(nfft / 100)
+hop_length = 1
 trans = T.Spectrogram(
-    n_fft=2048,
-    hop_length=1,  # One FFT per one video frame
+    n_fft=nfft,
+    hop_length=hop_length,
     normalized=True,
     power=2,
 )
 
 invert = T.GriffinLim(
-    n_fft=2048, n_iter=32, hop_length=1, win_length=2048, power=2
+    n_fft=nfft, n_iter=32, hop_length=hop_length, win_length=nfft, power=2
 )
 
-eod = torch.from_numpy(eod)
+todb = T.AmplitudeToDB(stype="power", top_db=80)
+
+eod = torch.from_numpy(eod).to(device)
 spec = trans(eod)
+spec_times = np.arange(0, spec.shape[1]) * hop_length / 44100
+spec_freqs = np.arange(0, spec.shape[0]) * 44100 / nfft
 
 # mask all upper frequencies
+
 spec_masked = spec.clone()
-spec_masked[:22, :] = 0
-spec_masked[24:, :] = 0
+for i, f in enumerate(bl1):
+    spec_masked[:, i][spec_freqs > f + 10] = 0
+    spec_masked[:, i][spec_freqs < f - 10] = 0
 
 eod_inv = invert(spec_masked)
+eod_inv_time = np.linspace(0, 1, len(eod_inv))
 inv_spec = trans(eod_inv)
 
-fig, axs = plt.subplots(5, 1)
+fig, axs = plt.subplots(5, 1, sharex=True)
 axs[0].plot(time, eod.numpy())
-axs[1].imshow(librosa.power_to_db(spec.numpy()), aspect="auto", origin="lower")
+axs[1].imshow(
+    todb(spec),
+    aspect="auto",
+    origin="lower",
+    extent=[spec_times[0], spec_times[-1], spec_freqs[0], spec_freqs[-1]],
+)
 axs[2].imshow(
-    librosa.power_to_db(spec_masked.numpy()), aspect="auto", origin="lower"
+    todb(spec_masked),
+    aspect="auto",
+    origin="lower",
+    extent=[spec_times[0], spec_times[-1], spec_freqs[0], spec_freqs[-1]],
 )
-axs[3].plot(time, eod_inv.numpy())
+axs[3].plot(eod_inv_time, eod_inv.numpy())
 axs[4].imshow(
-    librosa.power_to_db(inv_spec.numpy()), aspect="auto", origin="lower"
+    todb(inv_spec),
+    aspect="auto",
+    origin="lower",
+    extent=[spec_times[0], spec_times[-1], spec_freqs[0], spec_freqs[-1]],
 )
+axs[4].set_ylim([400, 900])
+axs[2].set_ylim([400, 900])
+axs[1].set_ylim([400, 900])
 plt.show()
